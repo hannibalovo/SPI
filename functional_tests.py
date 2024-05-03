efb0bb66c3e455c4686e44abf0b8df03f62809f7
hannibal <3045980445@qq.com>
2024-05-03 21:59:20 +0800

    sjz nineth commit : After refactoring the functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -29,8 +29,15 @@ from selenium.webdriver.common.by import By
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser= webdriver.Chrome()
+        
     def tearDown(self):
         self.browser.quit()
+        
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME,'tr')
+        self.assertIn(row_text, [row.test for row in rows])    
+        
     def test_can_start_a_list_and_retrieve_it_later(self):
         #张三听说有一个在线待办事项的应用#他去看了这个应用的首页
         self.browser.get('http://localhost:8000')
@@ -49,10 +56,30 @@ class NewVisitorTest(unittest.TestCase):
         #他按了回车键键后，页面更新了#待办事项表格中显示了“1:Buyflowers"
         inputbox.send_keys(Keys.ENTER)#(3)
         time.sleep(1)#(4)
-        table = self.browser.find_element(By.ID,'id_list_table')
-        rows = table.find_elements(By.TAG_NAME,'tr')#(1)
-        self.assertIn('1: Buy flowers', [row.text for row in rows])
-        #个文本输入框，可以输入其他待办事项#显示了#他输入了"gift togirlfriend"
+        
+        # 
+        # table = self.browser.find_element(By.ID,'id_list_table')
+        # rows = table.find_elements(By.TAG_NAME,'tr')#(1)
+        # self.assertIn('1: Buy flowers', [row.text for row in rows])
+        #个文本输入框，可以输入其他待办事项#显示了#他输入了"gift to girlfriend"
+        
+        self.check_for_row_in_list_table('1: Buy flowers')
+        
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        inputbox.send_keys('Give a gift to Lisi')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
+        # 页面再次更新，她的清单中显示了这两个待办事项
+        # table = self.browser.find_element(By.ID,'id_list_table')
+        # rows = table.find_elements(By.TAG_NAME,'tr')
+        # self.assertIn('1: Buy flowers',[row.text for row in rows])
+        # self.assertIn('2: Give a gift to Lisi',[row.text for row in rows])
+        # 
+        
+        self.check_for_row_in_list_table('1: Buy flowers')
+        self.check_for_row_in_list_table('2: Give a gift to Lisi')
+        
         self.fail('Finish the test!')
         #她的清单中显示了这两个待办事项
 if __name__ == '__main__':
--- lists/tests.py
+++ lists/tests.py
@@ -2,17 +2,27 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page #(1)
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
     
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')#(2)
-        self.assertEqual(found.func, home_page)#(3)
+    # def test_root_url_resolves_to_home_page_view(self):
+    #     found = resolve('/')#(2)
+    #     self.assertEqual(found.func, home_page)#(3)
     
-    def test_home_page_return_correct_html(self):
-        request = HttpRequest()#(1)
-        response =home_page(request)#(2)
-        html =response.content.decode('utf8')#(3)
-        self.assertTrue(html.startswith('<html>'))#(4)
-        self.assertIn('<title>To-Do lists</title>',html)
-        self.assertTrue(html.endswith('</html>'))#(4)
+    # def test_home_page_return_correct_html(self):
+    #     request = HttpRequest()#(1)
+    #     response =home_page(request)#(2)
+    #     html =response.content.decode('utf8')#(3)
+    #     self.assertTrue(html.startswith('<html>'))#(4)
+    #     self.assertIn('<title>To-Do lists</title>',html)
+    #     self.assertTrue(html.endswith('</html>'))#(4)
+    
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response,'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/',data={'item_text': 'A new list item'})
+        self.assertIn('A new list item',response.content.decode())
+        self.assertTemplateUsed(response,'home.html')
