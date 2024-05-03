8cf69a72de850c12bc4f153bf9a34d413769ad6e
hannibal <3045980445@qq.com>
2024-05-03 17:07:27 +0800

    sjz fourth commit: First unit test

--- test.py
+++ test.py
@@ -1,22 +1,22 @@
-from selenium import webdriver
-import unittest
+# from selenium import webdriver
+# import unittest
 
-class NewVisitorTest(unittest.TestCase):
+# class NewVisitorTest(unittest.TestCase):
 
-    def setUp(self):
-        self.browser = webdriver.Chrome()
+#     def setUp(self):
+#         self.browser = webdriver.Chrome()
 
-    def tearDown(self):
-        self.browser.quit()
+#     def tearDown(self):
+#         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
-    # 张三听说有一个在线待办事项的应用# 他去看了这个应用的首页
-        self.browser.get('http://localhost:8000')
-    #他注意到网页里包含"TO-Do"这个词
-        self.assertIn('To-Do',self.browser.title),"browser title was:"+ self.browser.title
-        self.fail('Finish the test!')
-    # 应用有一个输入待办事项的文本输入框
-    # 他在文本输入框中输入了“Buy flowers"
-    # 他访问那个URL，发现他的待办事项列表还在#他满意的离开了
-if __name__ == '__main__':
-    unittest.main()
+#     def test_can_start_a_list_and_retrieve_it_later(self):
+#     # 张三听说有一个在线待办事项的应用# 他去看了这个应用的首页
+#         self.browser.get('http://localhost:8000')
+#     #他注意到网页里包含"TO-Do"这个词
+#         self.assertIn('To-Do',self.browser.title),"browser title was:"+ self.browser.title
+#         self.fail('Finish the test!')
+#     # 应用有一个输入待办事项的文本输入框
+#     # 他在文本输入框中输入了“Buy flowers"
+#     # 他访问那个URL，发现他的待办事项列表还在#他满意的离开了
+# if __name__ == '__main__':
+#     unittest.main()
