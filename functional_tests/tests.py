2bd577237204796cb2f4f85c0114ffdbc5f82b3e
hannibal <3045980445@qq.com>
2024-05-04 15:06:34 +0800

    sjz thirteenth commit: refactor sleep()

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -26,6 +26,8 @@ import time
 import unittest
 from selenium.webdriver.common.by import By
 from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
+MAX_WAIT = 10
 
 # class NewVisitorTest(unittest.TestCase):
 class NewVisitorTest(LiveServerTestCase):
@@ -36,10 +38,23 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
         
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME,'tr')
-        self.assertIn(row_text, [row.text for row in rows])    
+    # def check_for_row_in_list_table(self, row_text):
+    def wait_for_row_in_list_table(self, row_text):
+        # table = self.browser.find_element(By.ID, 'id_list_table')
+        # rows = table.find_elements(By.TAG_NAME,'tr')
+        # self.assertIn(row_text, [row.text for row in rows]) 
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID,'id_list_table')#(3)
+                rows = table.find_elements(By.TAG_NAME,'tr')
+                self.assertIn(row_text,[row.text for row in rows])
+                return 
+            except (AssertionError, WebDriverException)as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+                   
         
     def test_can_start_a_list_and_retrieve_it_later(self):
         #张三听说有一个在线待办事项的应用#他去看了这个应用的首页
@@ -60,7 +75,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Buy flowers')#(2)
         #他按了回车键键后，页面更新了#待办事项表格中显示了“1:Buyflowers"
         inputbox.send_keys(Keys.ENTER)#(3)
-        time.sleep(1)#(4)
+        # time.sleep(1)#(4)
         
         # 
         # table = self.browser.find_element(By.ID,'id_list_table')
@@ -68,12 +83,12 @@ class NewVisitorTest(LiveServerTestCase):
         # self.assertIn('1: Buy flowers', [row.text for row in rows])
         #个文本输入框，可以输入其他待办事项#显示了#他输入了"gift to girlfriend"
         
-        self.check_for_row_in_list_table('1: Buy flowers')
+        self.wait_for_row_in_list_table('1: Buy flowers')
         
         inputbox = self.browser.find_element(By.ID,'id_new_item')
         inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        # time.sleep(1)
         
         # 页面再次更新，她的清单中显示了这两个待办事项
         # table = self.browser.find_element(By.ID,'id_list_table')
@@ -82,8 +97,8 @@ class NewVisitorTest(LiveServerTestCase):
         # self.assertIn('2: Give a gift to Lisi',[row.text for row in rows])
         # 
         
-        self.check_for_row_in_list_table('1: Buy flowers')
-        self.check_for_row_in_list_table('2: Give a gift to Lisi')
+        self.wait_for_row_in_list_table('1: Buy flowers')
+        self.wait_for_row_in_list_table('2: Give a gift to Lisi')
         
         self.fail('Finish the test!')
         #她的清单中显示了这两个待办事项
