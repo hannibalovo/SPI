5cc8b1450bbd483bd5e43d6ab7d5cbe9da6bd485
hannibal <3045980445@qq.com>
2024-05-04 15:29:01 +0800

    sjz fourteenth commit: add a new user

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -100,6 +100,43 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy flowers')
         self.wait_for_row_in_list_table('2: Give a gift to Lisi')
         
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # 张三新建一个待办事项清单
+        self.browser.get(self.live_server_url)
+        inputbox=self.browser.find_element(By.ID,'id_new_item')
+        inputbox.send_keys('Buy flowers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy flowers')
+        # 他注意到清单有个唯一的URL
+        zhangsan_list_url=self.browser.current_url
+        self.assertRegex(zhangsan_list_url,'/lists/.+')#(1)
+
+        # 个新用户王五访问网站杂 王不-#我们使用一个新浏览器会话#确保张三的信息不会从cookie中泄露出去
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
+        # 王五访问首页#页面中看不到张三的清单
+        self.browser.get(self.live_server_url)
+        page_text= self.browser.find_element(By.TAG_NAME,'body').text
+        self.assertNotIn('Buy flowers',page_text)
+        self.assertNotIn('BGive a gift to Lisi',page_text)
+        #王五输入个新待办事项项，新建一个个清单"id new item')
+        inputbox= self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        # 王五获得了他的唯-URL#
+        wangwu_list_url= self.browser.current_url
+        self.assertRegex(wangwu_list_url, '/lists/.+')
+        self.assertNotEqual(wangwu_list_url, zhangsan_list_url)
+        #这个面还是没有张三的清单= 
+        page_text=self.browser.find_element(By.TAG_NAME,'body').text
+        self.assertNotIn('Buy flowers',page_text)
+        self.assertIn('Buy milk',page_text)
+        #两人人都很满意，然后去睡觉了
+    
+        
+        
+        
         self.fail('Finish the test!')
         #她的清单中显示了这两个待办事项
         
