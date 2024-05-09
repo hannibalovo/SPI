0f59867157ca6f82232d329ed63c0b53f9ce8f1e
hannibal <3045980445@qq.com>
2024-05-09 13:54:13 +0800

    sjz twentieth commit: testing for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -138,5 +138,24 @@ class NewVisitorTest(LiveServerTestCase):
         # self.fail('Finish the test!')
         #她的清单中显示了这两个待办事项
         
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+# 她看到输入框完美地居中显示
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x']+ inputbox.size['width']/2,
+            512,delta=10
+            )
+        
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+# 她看到输入框完美地居中显示
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x']+ inputbox.size['width']/2,
+            512,delta=10
+            )
 # if __name__ == '__main__':
 #     unittest.main()
